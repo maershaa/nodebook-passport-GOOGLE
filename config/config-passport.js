0c02a5c56ee5a959c0f7c17ecb9d0cb77ef86ac6
maershaa <valeria.work2022@gmail.com>
2024-02-09 21:21:17 +0200

    Update config-passport.js

--- config/config-passport.js
+++ config/config-passport.js
@@ -46,7 +46,6 @@ passport.use(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      // callbackURL: 'http://localhost:3000/auth/google/callback',
       callbackURL: `${BASE_URL}/auth/google/callback` ,
       state: true // !хз надо ли
     },
